b3bc3cd992e5546b89bf83ad3b9b4a59d65e4f7f
Matt Bornski <matt@bornski.com>
2012-11-11 09:25:50 -0800

    Check in the sketch I used to test the watchdog

---
+++ watchdog_sketch/watchdog_sketch.ino
@@ -0,0 +1,48 @@
+int pulsePin = 8;
+unsigned long lastHeartbeat = 0;
+unsigned long lastUptimeReport = 0;
+
+void heartbeat() {
+  pinMode(pulsePin, OUTPUT);
+  delay(300);
+  pinMode(pulsePin, INPUT);
+  lastHeartbeat = millis();
+  Serial.println("Heartbeat sent");
+}
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pulse pin an input for high impedance
+  pinMode(pulsePin, INPUT);
+  
+  Serial.println("Arduino reset");
+  
+  // Send an initial heartbeat.
+  heartbeat();
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // Check for serial inputs.  If found, send heartbeat.
+  if (Serial.available()) {
+    // Clear input buffer
+    while (Serial.available()) {
+      Serial.read();
+    }
+    heartbeat();
+  }
+  unsigned long uptime = millis();
+  if ((uptime - lastUptimeReport) >= 5000) {
+    // It has been at least 5 seconds since our last uptime report.  
+    Serial.println("Uptime: " + String((uptime - (uptime % 5000)) / 1000) + " seconds (" + String((uptime - lastHeartbeat) / 1000) + " seconds since last heartbeat)");
+    // Pretend we did it exactly on the 5 second mark so we don't start slipping.
+    lastUptimeReport = (uptime - (uptime % 5000));
+  }
+  // delay in between loops
+  delay(100);
+}
+
+
+
